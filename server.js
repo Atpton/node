848259fd109b8aa320955c712b7942de23fef956
Atpton <35596884+Atpton@users.noreply.github.com>
2018-08-01 10:32:22 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
-const { dialogflow } =  require('actions-on-google'); 
-const bodyParser = require('body-parser');
 const express = require('express');
+var app = express();
 var port =process.env.PORT || 3000;
-let app = express();
-function appdialog(){
-    this.app = dialogflow({debug: true});
-    this.app.intent('hi', (conv) => {
-         conv.ask('Hello, World!');
-    console.log("OK get in intent");s
-    });
-} 
-
-let app1 = new appdialog();
-app.use(bodyParser.json());
-app.use("/",(req,res,next)=>{
-    console.log("get in.");
-    next();
-},app1.app);
+app.get('/',(req,res)=>{
+    res.send("HI");
+    console.log("Test");
+});
 app.listen(port);
 console.log("Create Server port :"+port);

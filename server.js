865f6b0fb0c625de4b04332b9b0068862e8d315e
atpton <s5704062636047@email.kmutnb.ac.th>
2018-08-01 21:24:53 +0700

    s

--- server.js
+++ server.js
@@ -6,16 +6,24 @@ let app = express();
 function appdialog(){
     this.app = dialogflow({debug: true});
     this.app.intent('Default Welcome Intent', (conv) => {
-         conv.ask('Hello I am ton. !');
-    console.log("OK get in intent");
+         conv.ask('\n Welcome to Your Nightmare. !');
+    console.log("OK get in Default intent ");
     });
-} 
+}
+function appdialog1(){
+  this.app = dialogflow({debug: true});
+  this.app.intent('hi', (conv) => {
+       conv.ask('Hello I am ton. !');
+  console.log("OK get in hi intent");
+  });
+}  
 
 let app1 = new appdialog();
+let app2 = new appdialog1();
 app.use(bodyParser.json());
 app.use("/",(req,res,next)=>{
     console.log("get in.");
     next();
-},app1.app);
+},[app1.app,app2.app]);
 app.listen(port);
 console.log("Create Server port :"+port);

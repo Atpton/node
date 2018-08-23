d5ba9319067c4220d356d509cd3701ca8831a31d
Atpton <35596884+Atpton@users.noreply.github.com>
2018-08-23 09:51:30 +0000

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,22 @@ var _http2 = _interopRequireDefault(_http);
 var app = _express();
 var port = process.env.APP_PORT||80;
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+_axios2.default.defaults.timeout = 6000;
+
+_axios2.default.interceptors.request.use(function (config) {
+    config.requestTime = new Date().getTime();
+    return config;
+}, function (err) {
+    return Promise.reject(err);
+});
+
+_axios2.default.interceptors.response.use(function (res) {
+  //  logger.logService({}, res.config, res, res.request.connection);
+    return res;
+}, function (err) {
+    //logger.logService(err, err.config, {}, err.request.connection);
+    return Promise.reject(err);
+});
 // app.post("/",function(req,res){
 //  res.json({name:`ton`});
 // });
@@ -37,7 +53,7 @@ async function testcall(req,res){
                     method: "display",
                     channel: "Google_Assistant",
                     intent: "message",
-                    timeout: 1000,
+                    timeout: 6000,
                     userId:"111111111"
                };
   var resData = await _axios2.default.post("https://dev-askaunjai.ais.co.th:8443/social-adapter-fe/chatbot",body,header);

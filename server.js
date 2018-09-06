0861b84e5ac0989c648fd6351a182f0995e03c97
Atpton <35596884+Atpton@users.noreply.github.com>
2018-09-06 21:56:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 var request1 = require('request').defaults({ encoding: null });
  const bodyParser = require('body-parser');
 var _https = require('https');
@@ -80,3 +80,68 @@ console.log("Create Server port :"+port);
 });
 //app.listen(port);
 //console.log("Create Server : "+port);
+*/
+
+const { dialogflow } =  require('actions-on-google'); 
+const bodyParser = require('body-parser');
+const express = require('express');
+const serveStatic = require('serve-static');
+var fs = require('fs');
+var { Image } = require('actions-on-google');
+var port =process.env.PORT || 3000;
+let app = express();
+app.use(serveStatic('./'));
+
+
+let https = require('https');
+
+
+ 
+let subscriptionKey = '31bbf02e5b6940b99e07a7e07acd4180';
+ 
+let host = 'api.cognitive.microsoft.com';
+let path = '/bing/v7.0/images/search';
+
+let term = 'liverpool';
+
+
+
+
+
+function appdialog(){
+    this.app = dialogflow({debug: true});
+    this.app.intent('Default Welcome Intent', (conv) => {
+         conv.ask('\n Welcome to Your Nightmare. !');
+    console.log("OK get in Default intent ");
+    });
+ 
+    this.app.intent('Default Fallback Intent',(conv)=>{
+     var googleUrl = "https://www.google.com/";
+              conv.ask(new BrowseCarousel({
+  items: [
+    new BrowseCarouselItem({
+      title: 'Title of item 1',
+      url: googleUrl,
+      description: 'Description of item 1',
+      footer: 'Item 1 footer',
+    }),
+    new BrowseCarouselItem({
+      title: 'Title of item 2',
+      url: googleUrl,
+      description: 'Description of item 2',
+      footer: 'Item 2 footer',
+    }),
+  ],
+}));
+    });
+}
+ 
+let app1 = new appdialog();
+app.use(bodyParser.json());
+app.use("/",(req,res,next)=>{
+    console.log("get in.");
+
+    next();
+},app1.app);
+app.listen(port);
+console.log("Create Server port :"+port);
